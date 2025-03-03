d86561cb5278baf75d6f1b7154b10035bbe4cffc
Pellumi <pellumii67@gmail.com>
2025-03-03 18:29:21 +0100

    Add new job data images and update API endpoints to production

--- client/src/actions/UserActions.js
+++ client/src/actions/UserActions.js
@@ -1,177 +1,198 @@
 import {
-    registerRequest, registerSuccess, registerFail, loginRequest, loginSuccess, loginFail
-    , isLoginRequest, isLoginSuccess, isLoginFail, getMeRequest, getMeSuccess, getMeFail,
-    changePasswordRequest, changePasswordSuccess, changePasswordFail,
-    updateProfileRequest, updateProfileSuccess, updateProfileFail,
-    deleteAccountRequest, deleteAccountSuccess, deleteAccountFail, logoutClearState
-} from '../slices/UserSlice'
-import { toast } from 'react-toastify'
-import axios from 'axios'
-
-
+  registerRequest,
+  registerSuccess,
+  registerFail,
+  loginRequest,
+  loginSuccess,
+  loginFail,
+  isLoginRequest,
+  isLoginSuccess,
+  isLoginFail,
+  getMeRequest,
+  getMeSuccess,
+  getMeFail,
+  changePasswordRequest,
+  changePasswordSuccess,
+  changePasswordFail,
+  updateProfileRequest,
+  updateProfileSuccess,
+  updateProfileFail,
+  deleteAccountRequest,
+  deleteAccountSuccess,
+  deleteAccountFail,
+  logoutClearState,
+} from "../slices/UserSlice";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 export const registerUser = (userData) => async (dispatch) => {
-    try {
-        dispatch(registerRequest())
+  try {
+    dispatch(registerRequest());
 
-        const { data } = await axios.post("http://localhost:3000/api/v1/register", userData);
+    const { data } = await axios.post(
+      "https://job-recruit-system.onrender.com/api/v1/register",
+      userData
+    );
 
-        dispatch(registerSuccess())
-        localStorage.setItem('userToken', data.token)
-        dispatch(logOrNot())
-        toast.success("Registration successful !")
-
-    } catch (err) {
-        dispatch(registerFail(err.response.data.message))
-        if (err.response.data.message.includes("duplicate")) {
-            toast.error("User already exists.")
-        } else {
-            toast.error(err.response.data.message)
-        }
+    dispatch(registerSuccess());
+    localStorage.setItem("userToken", data.token);
+    dispatch(logOrNot());
+    toast.success("Registration successful !");
+  } catch (err) {
+    dispatch(registerFail(err.response.data.message));
+    if (err.response.data.message.includes("duplicate")) {
+      toast.error("User already exists.");
+    } else {
+      toast.error(err.response.data.message);
     }
-}
-
+  }
+};
 
 export const loginUser = (userData) => async (dispatch) => {
-    try {
-        dispatch(loginRequest())
+  try {
+    dispatch(loginRequest());
 
-        const { data } = await axios.post("http://localhost:3000/api/v1/login", userData);
+    const { data } = await axios.post(
+      "https://job-recruit-system.onrender.com/api/v1/login",
+      userData
+    );
 
-        dispatch(loginSuccess())
-        localStorage.setItem('userToken', data.token)
-        dispatch(logOrNot())
-        dispatch(me())
-        toast.success("Login successful !")
-
-    } catch (err) {
-        dispatch(loginFail(err.response.data.message))
-        toast.error(err.response.data.message)
-    }
-}
-
+    dispatch(loginSuccess());
+    localStorage.setItem("userToken", data.token);
+    dispatch(logOrNot());
+    dispatch(me());
+    toast.success("Login successful !");
+  } catch (err) {
+    dispatch(loginFail(err.response.data.message));
+    toast.error(err.response.data.message);
+  }
+};
 
 export const logOrNot = () => async (dispatch) => {
-    try {
-        dispatch(isLoginRequest())
-        const config = {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('userToken')}`
-            }
-        }
+  try {
+    dispatch(isLoginRequest());
+    const config = {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+      },
+    };
 
-        const { data } = await axios.get("http://localhost:3000/api/v1/isLogin", config);
+    const { data } = await axios.get(
+      "https://job-recruit-system.onrender.com/api/v1/isLogin",
+      config
+    );
 
-        dispatch(isLoginSuccess(data.isLogin))
-
-
-
-    } catch (err) {
-        dispatch(isLoginFail())
-    }
-}
-
+    dispatch(isLoginSuccess(data.isLogin));
+  } catch (err) {
+    dispatch(isLoginFail());
+  }
+};
 
 export const me = () => async (dispatch) => {
-    try {
-        dispatch(getMeRequest())
-        const config = {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('userToken')}`
-            }
-        }
+  console.log("i ran me: ");
+  try {
+    dispatch(getMeRequest());
+    const config = {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+      },
+    };
 
-        const { data } = await axios.get("http://localhost:3000/api/v1/me", config);
-        
-        localStorage.setItem("role", data.user.role)
+    const { data } = await axios.get(
+      "https://job-recruit-system.onrender.com/api/v1/me",
+      config
+    );
 
-        dispatch(getMeSuccess(data.user))
+    localStorage.setItem("role", data.user.role);
 
-    } catch (err) {
-        dispatch(getMeFail())
-    }
-}
+    console.log("me: ", data.user);
 
+    dispatch(getMeSuccess(data.user));
+  } catch (err) {
+    dispatch(getMeFail());
+  }
+};
 
 export const changePass = (userData) => async (dispatch) => {
-    try {
-        dispatch(changePasswordRequest())
+  try {
+    dispatch(changePasswordRequest());
 
-        const config = {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('userToken')}`
-            }
-        }
+    const config = {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+      },
+    };
 
-        const { data } = await axios.put("http://localhost:3000/api/v1/changePassword", userData, config)
+    const { data } = await axios.put(
+      "https://job-recruit-system.onrender.com/api/v1/changePassword",
+      userData,
+      config
+    );
 
-        dispatch(changePasswordSuccess())
-        toast.success("Password Changed successfully !")
-
-    } catch (err) {
-        dispatch(changePasswordFail(err.response.data.message))
-        toast.error(err.response.data.message)
-    }
-}
-
+    dispatch(changePasswordSuccess());
+    toast.success("Password Changed successfully !");
+  } catch (err) {
+    dispatch(changePasswordFail(err.response.data.message));
+    toast.error(err.response.data.message);
+  }
+};
 
 export const updateProfile = (userData) => async (dispatch) => {
-    try {
-        dispatch(updateProfileRequest())
+  try {
+    dispatch(updateProfileRequest());
 
-        const config = {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('userToken')}`
-            }
-        }
+    const config = {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+      },
+    };
 
-        const { data } = await axios.put("http://localhost:3000/api/v1/updateProfile", userData, config)
+    const { data } = await axios.put(
+      "https://job-recruit-system.onrender.com/api/v1/updateProfile",
+      userData,
+      config
+    );
 
-        dispatch(updateProfileSuccess())
-        toast.success("Profile Updated successfully !")
-        dispatch(me())
-
-    } catch (err) {
-        dispatch(updateProfileFail(err.response.data.message))
-        toast.error(err.response.data.message)
-    }
-}
-
+    dispatch(updateProfileSuccess());
+    toast.success("Profile Updated successfully !");
+    dispatch(me());
+  } catch (err) {
+    dispatch(updateProfileFail(err.response.data.message));
+    toast.error(err.response.data.message);
+  }
+};
 
 export const deleteAccount = (userData) => async (dispatch) => {
-    try {
-        console.log(userData)
+  try {
+    console.log(userData);
 
+    dispatch(deleteAccountRequest());
 
-        dispatch(deleteAccountRequest())
+    const config = {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+      },
+    };
 
-        const config = {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('userToken')}`
-            }
-        }
+    const { data } = await axios.put(
+      "https://job-recruit-system.onrender.com/api/v1/deleteAccount",
+      userData,
+      config
+    );
 
-        const { data } = await axios.put("http://localhost:3000/api/v1/deleteAccount", userData, config)
+    console.log(data);
 
-        console.log(data)
-
-        dispatch(deleteAccountSuccess())
-        if (data.message === "Account Deleted") {
-            toast.success("Account Deleted successfully !")
-            localStorage.removeItem('userToken')
-            dispatch(logOrNot())
-            dispatch(logoutClearState())
-        }else{
-            toast.error("Wrong Password !")
-        }
-
-
+    dispatch(deleteAccountSuccess());
+    if (data.message === "Account Deleted") {
+      toast.success("Account Deleted successfully !");
+      localStorage.removeItem("userToken");
+      dispatch(logOrNot());
+      dispatch(logoutClearState());
+    } else {
+      toast.error("Wrong Password !");
     }
-    catch (err) {
-        dispatch(deleteAccountFail(err.response.data.message))
-        toast.error(err.response.data.message)
-    }
-}
-
-
-
+  } catch (err) {
+    dispatch(deleteAccountFail(err.response.data.message));
+    toast.error(err.response.data.message);
+  }
+};
