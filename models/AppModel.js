efa6b374cbeae41a407d93c54bd42aea3ae1dfdd
Pellumi <pellumii67@gmail.com>
2025-03-20 08:06:33 +0100

    neneneneen

--- models/AppModel.js
+++ models/AppModel.js
@@ -1,36 +1,36 @@
-const mongoose = require('mongoose') ;
+const mongoose = require("mongoose");
 
-const ApplicationSchema =  new mongoose.Schema({
-    job: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Job',
-        required: true
+const ApplicationSchema = new mongoose.Schema({
+  job: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Job",
+    required: true,
+  },
+  applicant: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  applicantResume: {
+    public_id: {
+      type: String,
+      required: true,
     },
-    applicant: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+    url: {
+      type: String,
+      required: true,
     },
-    applicantResume: {
-        public_id: {
-            type: String,
-            required: true
-        },
-        url: {
-            type: String,
-            required: true
-        }
-    }, 
-    status: {
-        type: String,
-        enum: ['pending', 'accepted', 'rejected'],
-        default: 'pending'
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-})
+  },
+  status: {
+    type: String,
+    enum: ["pending", "accepted", "interview", "rejected"],
+    default: "pending",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const Application = mongoose.model('Application', ApplicationSchema) ;
-module.exports = Application
+const Application = mongoose.model("Application", ApplicationSchema);
+module.exports = Application;
